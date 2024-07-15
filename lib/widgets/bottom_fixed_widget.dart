5ef86ae65faba2f2ea88a9147fbb347a6d2e44bd
Benny-glitch <58368441+Benny-glitch@users.noreply.github.com>
2024-07-15 16:57:55 +0200

    Created the bottom widget for create the collection

---
+++ lib/widgets/bottom_fixed_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomFixedWidget extends StatelessWidget {
+  const BottomFixedWidget({super.key});
+  static const double height = 120.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.13,
+      color: Colors.white,
+      padding: const EdgeInsets.fromLTRB(16.0,16.0,16.0,0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Row(
+            children: [
+              Icon(Icons.folder, color: Colors.orange),
+              SizedBox(width: 8),
+              Text('Create new collection'),
+              Spacer(),
+              Icon(Icons.add, color: Colors.orange),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              Expanded(
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: Text('Explore'),
+                ),
+              ),
+              const SizedBox(width: 8),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Collection'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
