5944f1cdc5dbb6ea271ac64eb3ec3a8cbbf5c6d3
Benny-glitch <58368441+Benny-glitch@users.noreply.github.com>
2024-09-05 16:36:35 +0200

    added the typeahead for the search bar and created the search bar with suggestions

---
+++ lib/widgets/collection_page/search_bar.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import '../../models/card.dart';
+//import '../../services/card_service.dart';
+
+class AutoCompleteSearchApp extends StatefulWidget {
+  @override
+  _AutoCompleteSearchAppState createState() => _AutoCompleteSearchAppState();
+}
+
+class _AutoCompleteSearchAppState extends State<AutoCompleteSearchApp> {
+  final TextEditingController _controller = TextEditingController();
+  //final CardService _cardService = CardService();
+  Timer? _debounce;
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TypeAheadField<PokemonCard?>(
+      textFieldConfiguration: TextFieldConfiguration(
+        autofocus: true,
+        controller: _controller,
+        decoration: const InputDecoration(
+          hintText: 'Search for a card...',
+        ),
+      ),
+      suggestionsCallback: (pattern) async {
+        if (pattern.isEmpty) {
+          return [];
+        }
+
+        setState(() {
+          _isLoading = true;
+        });
+
+        if (_debounce?.isActive ?? false) {
+          _debounce?.cancel();
+        }
+
+        final completer = Completer<Iterable<PokemonCard?>>();
+
+        /*_debounce = Timer(const Duration(milliseconds: 300), () async {
+          List<PokemonCard>? cards = await _cardService.searchCards(pattern);
+
+          final Set<String> seenNames = <String>{};
+          final List<PokemonCard?> uniqueCards = cards
+              .where((card) {
+            final isUnique = seenNames.add(card.name);
+            return isUnique;
+          })
+              .toList();
+
+          setState(() {
+            _isLoading = false;
+          });
+
+          completer.complete(uniqueCards);
+        });*/
+        return completer.future;
+      },
+      itemBuilder: (context, PokemonCard? suggestion) {
+        if (suggestion == null) {
+          return const SizedBox();
+        }
+        return ListTile(
+          title: Text(suggestion.name),
+        );
+      },
+      noItemsFoundBuilder: (context) {
+        if (_isLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        return const ListTile(
+          title: Text('Nessuna carta trovata'),
+        );
+      },
+      suggestionsBoxDecoration: SuggestionsBoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        color: Colors.white,
+        elevation: 4.0,
+      ),
+      onSuggestionSelected: (PokemonCard? suggestion) {
+        if (suggestion != null) {
+          _controller.text = suggestion.name;
+        }
+      },
+    );
+  }
+}
