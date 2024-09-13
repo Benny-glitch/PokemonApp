cd6873f6f202be535ca8075b14121f48ca85b32b
Benny-glitch <58368441+Benny-glitch@users.noreply.github.com>
2024-09-13 22:15:56 +0200

    Updated the method for research a card

--- lib/services/card_service.dart
+++ lib/services/card_service.dart
@@ -7,34 +7,26 @@ class CardService {
   final Dio _dio = WebUtil.createDio();
   final String _baseUrl = Utils.baseUrl;
 
-  Future<List<PokemonCard>> searchCards(String name) async {
-    try {
-      final response = await _dio.get(
-        '$_baseUrl/cards',
-        queryParameters: {'q': 'name:$name*'},
-      );
-
-      if (response.statusCode == 200) {
-        final data = response.data;
-
-        // Ensure data and data['data'] are not null and are of the correct type
-        if (data != null && data['data'] != null && data['data'] is List) {
-          final List<dynamic> cardsJson = data['data'];
-          // Convert JSON to List of PokemonCard objects, ignoring nulls
-          final List<PokemonCard> cards = cardsJson
-              .map((json) => PokemonCard.fromJson(json as Map<String, dynamic>))
-              .toList();
-          return cards;
-        } else {
-          // Return an empty list if data is not in the expected format
-          return [];
+  Stream<List<PokemonCard>> searchCards(String name) async* {
+    List<PokemonCard> cards = [];
+    int page = 1;
+    while(page < 6){
+      try {
+        final response = await _dio.get(
+          '$_baseUrl/cards',
+          queryParameters: {
+            'q': 'name:$name*',
+            'pageSize': 5,
+            'page': page,
+          },
+        );
+        for(var card in response.data['data']){
+          cards.add(PokemonCard.fromJson(card as Map<String, dynamic>));
+          yield cards;
         }
-      } else {
-        throw Exception('Failed to load cards: ${response.statusMessage}');
+      }catch(e){
       }
-    } catch (e) {
-      print('Error fetching cards: $e');
-      return [];
+      page += 1;
     }
   }
 }
