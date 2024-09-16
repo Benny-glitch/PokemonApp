5d7dc6cf8818d9d13f2dc83b4526898e63b658be
Benny-glitch <58368441+Benny-glitch@users.noreply.github.com>
2024-09-16 19:14:26 +0200

    Adjusted the view model of the single item of the search

--- lib/widgets/collection_page/search_bar.dart
+++ lib/widgets/collection_page/search_bar.dart
@@ -5,7 +5,8 @@ import '../../services/card_service.dart';
 
 class AutoCompleteSearchWidget extends StatefulWidget {
   @override
-  _AutoCompleteSearchWidgetState createState() => _AutoCompleteSearchWidgetState();
+  _AutoCompleteSearchWidgetState createState() =>
+      _AutoCompleteSearchWidgetState();
 }
 
 class _AutoCompleteSearchWidgetState extends State<AutoCompleteSearchWidget> {
@@ -80,7 +81,6 @@ class _AutoCompleteSearchWidgetState extends State<AutoCompleteSearchWidget> {
       _suggestions = [];
     });
 
-
     Future.delayed(Duration(milliseconds: 100), () {
       _controller.addListener(_onSearchChanged);
       _isSuggestionSelected = false;
@@ -92,7 +92,7 @@ class _AutoCompleteSearchWidgetState extends State<AutoCompleteSearchWidget> {
     return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 0),
           child: TextField(
             controller: _controller,
             decoration: const InputDecoration(
@@ -102,57 +102,145 @@ class _AutoCompleteSearchWidgetState extends State<AutoCompleteSearchWidget> {
             ),
           ),
         ),
-        AnimatedContainer(
-          duration: Duration(milliseconds: 300),
-          curve: Curves.easeInOut,
-          constraints: _suggestions.isEmpty
-              ? BoxConstraints(maxHeight: 0)
-              : BoxConstraints(maxHeight: 200),
-          child: _suggestions.isEmpty
-              ? SizedBox.shrink()
-              : ListView.builder(
-            itemCount: _suggestions.length,
-            itemBuilder: (context, index) {
-              final card = _suggestions[index];
-              return Column(
-                children: [
-                  ListTile(
-                    leading: Container(
-                      width: 50,
-                      height: 50,
-                      child:  card.images!.small.toString().isNotEmpty
-                          ? Image.network(
-                        card.images!.small.toString(),
-                        width: 50,
-                        height: 50,
-                        fit: BoxFit.cover,
-                        errorBuilder: (context, error, stackTrace) {
-                          return Container(
-                            color: Colors.grey[200],
-                            child: Icon(
-                              Icons.error,
-                              color: Colors.red,
-                              size: 24,
-                            ),
-                          );
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.grey.shade50,
+              border: Border(
+                  top: BorderSide(color: Colors.grey.shade100, width: 1.0),
+                  bottom: BorderSide(color: Colors.grey.shade100, width: 1.0))),
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            constraints: _suggestions.isEmpty
+                ? BoxConstraints(maxHeight: 0)
+                : BoxConstraints(maxHeight: 500),
+            child: _suggestions.isEmpty
+                ? SizedBox.shrink()
+                : ListView.builder(
+                    itemCount: _suggestions.length,
+                    itemBuilder: (context, index) {
+                      final card = _suggestions[index];
+                      return GestureDetector(
+                        onTap: () {
+                          _onSuggestionSelected(card);
                         },
-                      )
-                          : Container(
-                        color: Colors.grey[200],
-                        child: Icon(
-                          Icons.error,
-                          color: Colors.red,
-                          size: 36,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: Container(
+                            margin: EdgeInsets.symmetric(horizontal: 16.0),
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey.shade300),
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.white),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: AspectRatio(
+                                      aspectRatio: 0.7,
+                                      child: card.images!.small
+                                              .toString()
+                                              .isNotEmpty
+                                          ? ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              child: Image.network(
+                                                card.images!.small.toString(),
+                                                fit: BoxFit.cover,
+                                                errorBuilder: (context, error,
+                                                    stackTrace) {
+                                                  return Container(
+                                                    color: Colors.grey[200],
+                                                    child: Icon(
+                                                      Icons.error,
+                                                      color: Colors.red,
+                                                      size: 48,
+                                                    ),
+                                                  );
+                                                },
+                                              ),
+                                            )
+                                          : Container(
+                                              color: Colors.grey[200],
+                                              child: Icon(
+                                                Icons.error,
+                                                color: Colors.red,
+                                                size: 48,
+                                              ),
+                                            ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 13,
+                                  ),
+                                  Expanded(
+                                    flex: 5, // Spazio maggiore per i dettagli
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          card.name,
+                                          style: TextStyle(
+                                            fontSize: 20, // Nome più grande
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        SizedBox(height: 5),
+                                        Text(
+                                          '${card.set?.name}',
+                                          // Aggiungi collezione qui
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.grey[600],
+                                          ),
+                                        ),
+                                        Align(
+                                            alignment: Alignment.bottomCenter,
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                Text(
+                                                  '${card.id}',
+                                                  style: TextStyle(
+                                                    fontSize: 20,
+                                                    color: Colors.grey[600],
+                                                  ),
+                                                ),
+                                                card.cardmarket?.prices
+                                                            ?.averageSellPrice !=
+                                                        null
+                                                    ? Text(
+                                                        '${card.cardmarket?.prices?.averageSellPrice}',
+                                                        style: TextStyle(
+                                                          fontSize: 20,
+                                                          color:
+                                                              Colors.grey[600],
+                                                        ),
+                                                      )
+                                                    : Icon(
+                                                        Icons.error,
+                                                        color: Colors.red,
+                                                        size: 48,
+                                                      ),
+                                              ],
+                                            )),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    title: Text(card.name),
-                    onTap: () => _onSuggestionSelected(card),
+                      );
+                    },
                   ),
-                  SizedBox(height: 8),
-                ],
-              );
-            },
           ),
         ),
       ],
